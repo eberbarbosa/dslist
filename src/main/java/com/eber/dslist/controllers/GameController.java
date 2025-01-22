1e2e109983cf7866a99ad1d239190c84f71be2bd
Eber Barbosa <eberbarbosa255@gmail.com>
2025-01-21 21:28:43 -0300

    GET game by id

--- src/main/java/com/eber/dslist/controllers/GameController.java
+++ src/main/java/com/eber/dslist/controllers/GameController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eber.dslist.dto.GameDTO;
 import com.eber.dslist.dto.GameMinDTO;
 import com.eber.dslist.entities.Game;
 import com.eber.dslist.services.GameService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -20,14 +22,18 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {		
+		GameDTO result = gameService.findById(id);	
+		return result;
+	}
+	
 	
 	@GetMapping
 	public List<GameMinDTO> findAll() {
 		
-		List<GameMinDTO> result = gameService.findAll();
-		
+		List<GameMinDTO> result = gameService.findAll();	
 		return result;
-
 	}
 	
 
