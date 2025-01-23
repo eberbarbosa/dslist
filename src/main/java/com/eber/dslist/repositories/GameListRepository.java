2dcdd1a84cc1a803f953ad53b68a6e6d9eb4a8ce
Eber Barbosa <eberbarbosa255@gmail.com>
2025-01-23 17:33:51 -0300

    Move game list

--- src/main/java/com/eber/dslist/repositories/GameListRepository.java
+++ src/main/java/com/eber/dslist/repositories/GameListRepository.java
@@ -1,14 +1,17 @@
 package com.eber.dslist.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.eber.dslist.entities.GameList;
-import com.eber.dslist.projections.GameMinProjection;
 
 public interface GameListRepository  extends JpaRepository<GameList, Long> {
+	
+	@Modifying
+	@Query(nativeQuery = true, 
+		value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 	
 	
 }
